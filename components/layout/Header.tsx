de5515addb7885159af782b22415029edb722bd3
PrathamTelang <prathamtelang007@gmail.com>
2026-01-08 20:37:27 +0530

    Redesign Header layout and add search input
    Updated the Header component with a new layout, including a search input field, improved styling, and branding. The header now uses a white background, increased height, and better alignment for elements.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,10 +4,14 @@ import { FaCircle } from 'react-icons/fa'
 
 function Header() {
   return (
-    <div className='w-screen bg-red-700 sm:h-16'>
-        <div><FaCircle /></div>
-        <div>
-            <div><CiShoppingCart /></div>
+    <div className='w-screen bg-white sm:h-20 flex justify-between items-center px-8 text-black border-b border-gray-300 py-4 shadow'>
+        <div className='flex justify-center items-center gap-2'>
+          <FaCircle className='text-2xl text-orange-600' />
+          <h1 className='font-bold text-xl tracking-widest'>WHATBYTES</h1>
+        </div>
+        <div className='flex justify-center items-center gap-2'>
+            <input className='border sm:w-96 border-gray-300 shadow  rounded-md px-4 py-2' placeholder="Search for products" />
+            <CiShoppingCart className='bg-[#18181B] text-white rounded-full text-5xl p-2 cursor-pointer' />
         </div>
     </div>
   )
