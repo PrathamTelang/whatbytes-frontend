a46430b99aa32410b82c919213ef6da54c5c5a81
PrathamTelang <prathamtelang007@gmail.com>
2026-01-09 13:35:30 +0530

    Make onChange prop required in Searchbar
    Updated the Searchbar component to require the onChange prop, removing its optional status. Also simplified the onChange handler to call the function directly.

--- components/filters/Searchbar.tsx
+++ components/filters/Searchbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 type SearchbarProps = {
-    value: string;
-    onChange?: (value: string) => void;
-}
+  value: string;
+  onChange: (value: string) => void;
+};
+
 
 function Searchbar({ value, onChange }: SearchbarProps) {
   return (
@@ -11,7 +12,7 @@ function Searchbar({ value, onChange }: SearchbarProps) {
               <input 
               className='border w-full border-gray-300 shadow  rounded-md px-4 py-2 focus:outline-none focus:shadow-lg' placeholder="Search for products" 
               value={value} 
-              onChange={(e) => onChange && onChange(e.target.value)} />
+              onChange={(e) => onChange(e.target.value)} />
             </div>
   )
 }
